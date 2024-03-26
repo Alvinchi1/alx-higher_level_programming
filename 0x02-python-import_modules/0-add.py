4d34f5655bcc8efc45c1411e381096542db1d7a7
Alvinchi1 <alvinchibundo007>
2024-03-26 10:02:47 -0700

    import modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+#ths code will print the value of a+b 
+#from add_0 import add
+
+if __name__ == "__main__":
+    from add_0 import add
+        a = 1
+        b = 2
+
+         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if _name_ == "_main_":
+    import sys
+    i = len(sys.argv) - 1
+
+
+    if i == 0:
+        print("{} argument.".format(i))
+    elif i == 1:
+        print(("{} argument:".format(i)
+    else:
+        print(print("{} arguments:".format)(i)
+    
+    if i >= 1:
+    i = 0
+    for arg in sys.argv:
+        if i != 0:
+            print("{} : {}".fomat(i argv))
+        i == 1
